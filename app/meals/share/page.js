fb29d52fbdf4fc2e7b869a33edb5a5c71639e5e8
Zach Hudson <hudson@Zachs-MacBook-Pro.local>
2024-02-08 10:01:23 -0600

    first commit

---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMeal() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>Share Page</h1>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Hmmmm...</h1>
+      <p>It doesn't look like that page exists. Maybe try another Page?</p>
+    </main>
+  );
+}
