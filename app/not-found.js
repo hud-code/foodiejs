c3cda266328801226b31a384625e5fbfb122abe5
Zach Hudson <hudson@Zachs-MacBook-Pro.local>
2024-02-08 10:16:10 -0600

    fixing apostraphe

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Hmmmm...</h1>
-      <p>It doesn't look like that page exists. Maybe try another Page?</p>
+      <p>It doesn&apos;t look like that page exists. Maybe try another Page?</p>
     </main>
   );
 }
