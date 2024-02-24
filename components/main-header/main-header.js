55c20b498822e9017bb7cceeafc99f484c5e426f
Zach Hudson <hudson@Zachs-MacBook-Pro.local>
2024-02-23 21:05:45 -0600

    this is adding bretts name

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import classes from "./main-header.module.css";
+import Link from 'next/link';
+import classes from './main-header.module.css';
 
-import logoImg from "@/assets/logo.png";
-import Image from "next/image";
-import MainHeaderBackground from "./main-header-background";
-import NavLink from "./nav-link";
+import logoImg from '@/assets/logo.png';
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -13,7 +13,7 @@ export default function MainHeader() {
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="A plate with food on it" priority />
-          NextLevel Foods
+          NextLevel Brett
         </Link>
 
         <nav className={classes.nav}>
